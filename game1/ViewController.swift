caef5a1e72fc04b983e6e219a40043b57207b29b
TearsOprah <qmeshokp@mail.ru>
2022-05-28 11:18:52 +0300

    логика переворачивания кнопок

--- game1/ViewController.swift
+++ game1/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let emojiCollection = ["🦐", "🐍", "🦐", "🐍"]
+    
+    var touches = 0 {
+        didSet {
+            touchesLabel.text = "Touches: \(touches)"
+        }
     }
+    
+    func flipButton (emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+    @IBOutlet var buttonCollection: [UIButton]!
+    
+    
+    @IBOutlet weak var touchesLabel: UILabel!
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            flipButton(emoji: emojiCollection[buttonIndex], button: sender)
+        }
 
-
+    }
 }
 
